a4ef3355e41a405947069e71fdd9641e809f48ee
jimmyswrld <131459716+jimmyswrld@users.noreply.github.com>
2024-05-09 08:48:24 +0100

    Update v2.js

--- v2.js
+++ v2.js
@@ -1,107 +1,173 @@
-document.addEventListener("DOMContentLoaded", function () {
-  // External library for advanced math expression parsing and evaluation
-  const mathjs = window.math;
+(function () {
+  // Function to load an external script
+  function loadScript(url, callback) {
+    var script = document.createElement("script");
+    script.type = "text/javascript";
+    script.src = url;
 
-  // Function to evaluate expressions with variable substitution
-  const evaluateExpression = (expression, variables) => {
-    const preparedExpression = expression.replace(/{{(.*?)}}/g, (_, match) =>
-      match.trim()
-    );
-    let parser = mathjs.parser();
+    script.onreadystatechange = callback;
+    script.onload = callback;
 
-    // Assign variables
-    for (let [variable, value] of variables) {
-      parser.set(variable, value);
-    }
+    document.head.appendChild(script);
+  }
 
-    // Evaluate the expression
-    let result = parser.evaluate(preparedExpression);
-    console.log(`Evaluated: ${preparedExpression} = ${result}`);
-    return result;
-  };
+  // Load math.js
+  loadScript(
+    "https://cdn.jsdelivr.net/npm/mathjs@11.9.1/lib/browser/math.min.js",
+    function () {
+      console.log("math.js is loaded!");
+      // Initialize your calculator logic here after math.js has loaded
+      // Your calculator code here
+      const mathjs = window.math;
 
-  const inputs = document.querySelectorAll("[j-calc-input]");
-  const outputs = document.querySelectorAll("[j-calc-output]");
+      function evaluateExpression(expression) {
+        let inputNames = getExpressionVariables(expression);
 
-  let variableMap = new Map();
+        let parser = mathjs.parser();
 
-  inputs.forEach((input) => {
-    const inputName = input.getAttribute("j-calc-input");
+        inputNames.forEach((inputName) => {
+          const inputEl = document.querySelector(`input[name="${inputName}"]`);
 
-    if (variableMap.has(inputName)) {
-      console.error(`Duplicate input name found: ${inputName}`);
-      return;
-    }
+          if (inputEl) {
+            const inputValue = getInputValue(inputEl);
+            // getInputValue(inputEl);
 
-    let inputValue;
-    if (input.getAttribute("type") === "checkbox") {
-      inputValue = input.checked
-        ? parseFloat(input.getAttribute("j-calc-checkbox-value")) || 0
-        : parseFloat(input.getAttribute("j-calc-default-value")) || 0;
-    } else {
-      inputValue =
-        parseFloat(input.value) ||
-        parseFloat(input.getAttribute("j-calc-default-value")) ||
-        0;
-    }
-    variableMap.set(inputName, inputValue);
-  });
+            parser.set(inputName, inputValue);
+          }
+        });
 
-  inputs.forEach((input) => {
-    input.addEventListener("input", () => {
-      const inputName = input.getAttribute("j-calc-input");
-      let inputValue;
-      if (input.getAttribute("type") === "checkbox") {
-        inputValue = input.checked
-          ? parseFloat(input.getAttribute("j-calc-checkbox-value")) || 0
-          : parseFloat(input.getAttribute("j-calc-default-value")) || 0;
-      } else {
-        inputValue =
-          parseFloat(input.value) ||
-          parseFloat(input.getAttribute("j-calc-default-value")) ||
-          0;
+        let result = parser.evaluate(expression);
+
+        return result;
       }
-      variableMap.set(inputName, inputValue);
 
-      outputs.forEach((output) => {
-        if (output.getAttribute("j-calc-on-submit")) {
-          return; // Skip to the next iteration if the output is only updated on submit
-        }
-        const operation = output.getAttribute("j-calc-output");
-        const result = evaluateExpression(operation, variableMap);
+      function getExpressionVariables(expression) {
+        let node = mathjs.parse(expression);
+        // Filter for SymbolNodes, which represent variables
+        let variables = node.filter((node) => node.isSymbolNode);
 
-        const format = output.getAttribute("j-calc-format");
-        if (format === "currency") {
-          output.textContent = result.toFixed(2);
-        } else {
-          output.textContent = result;
+        // Extract the names of the variables and return
+        return variables.map((variable) => variable.name);
+      }
+
+      function getInputValue(inputElement) {
+        const inputType = inputElement.type;
+
+        let inputValue;
+
+        switch (inputType) {
+          case "radio":
+            let selectedRadioValue = document.querySelector(
+              `input[name="${inputElement.name}"]:checked`
+            ).value;
+
+            inputValue = Number(selectedRadioValue) || 0;
+            //console.log(inputValue)
+            break;
+          case "checkbox":
+            inputValue = inputElement.checked
+              ? Number(inputElement.getAttribute("j-calc-checkbox"))
+              : 0;
+            //console.log(inputValue)
+            break;
+
+          default:
+            inputValue = inputElement.valueAsNumber || 0;
+
+          //console.log(inputValue)
         }
+
+        if (inputElement.getAttribute("j-calc-default") && !inputValue) {
+          inputValue = Number(inputElement.getAttribute("j-calc-default"));
+          //console.log(inputValue)
+        }
+
+        return inputValue;
+      }
+
+      const outputs = document.querySelectorAll("[j-calc-output]");
+
+      const firstAtt = outputs[0].getAttribute("j-calc-on-submit");
+
+      const allInputs = document.querySelectorAll("input");
+
+      allInputs.forEach((input) => {
+        input.addEventListener("input", () => {
+          outputs.forEach((output) => {
+            if (!output.getAttribute("j-calc-submit-output")) {
+              updateOutput(output);
+            }
+          });
+        });
       });
-    });
-  });
 
-  const submitButtons = document.querySelectorAll("[j-calc-submit-btn]");
+      const submitBtns = document.querySelectorAll("[j-calc-submit-btn]");
 
-  submitButtons.forEach((button) => {
-    button.addEventListener("click", (event) => {
-      event.preventDefault();
+      //console.log(submitBtns);
 
-      const linkName = button.getAttribute("j-calc-submit-btn");
-      outputs.forEach((output) => {
-        if (output.getAttribute("j-calc-on-submit") !== linkName) {
-          return; // Skip to the next iteration if the output doesn't match the link name
-        }
-        const operation = output.getAttribute("j-calc-output");
-        const result = evaluateExpression(operation, variableMap);
+      submitBtns.forEach((btn) => {
+        btn.addEventListener("click", () => {
+          const linkValue = btn.getAttribute("j-calc-submit-btn");
+          const linkedOutput = document.querySelectorAll(
+            `[j-calc-submit-output="${linkValue}"]`
+          );
 
-        const format = output.getAttribute("j-calc-format");
-        if (format === "currency") {
-          output.textContent = result.toFixed(2);
-        } else {
-          output.textContent = result;
-        }
+          //console.log(linkedOutput);
+
+          if (linkedOutput.length === 0) {
+            console.log("No matching output!");
+            return;
+          }
+
+          linkedOutput.forEach((output) => {
+            updateOutput(output);
+          });
+        });
       });
-    });
-  });
-});
 
+      function updateOutput(output) {
+        const decimalPlaces = output.getAttribute("j-calc-dp");
+
+        const outputType = output.tagName;
+
+        const roundTo = output.getAttribute("j-calc-round");
+
+        let outputValue = evaluateExpression(
+          output.getAttribute("j-calc-output")
+        );
+
+        if (roundTo) {
+          outputValue = flexibleRound(outputValue, roundTo);
+        }
+
+        if (decimalPlaces) {
+          outputValue = outputValue.toFixed(Number(decimalPlaces));
+        }
+
+        if (outputType === "INPUT") {
+          output.value = outputValue;
+        } else {
+          output.innerText = outputValue;
+        }
+      }
+
+      function roundToNearestX(num, x) {
+        x = Number(x);
+        return Math.round(num / x) * x;
+      }
+
+      function flexibleRound(num, unit) {
+        // Determine if we're dealing with decimals or whole numbers
+        if (unit >= 1) {
+          // Rounding to nearest whole number unit (e.g., 10, 100)
+          return Math.round(num / unit) * unit;
+        } else {
+          // Rounding to a decimal place (e.g., 0.1, 0.01)
+          let decimalPlaces = Math.abs(Math.log10(unit));
+          let scaleFactor = Math.pow(10, decimalPlaces);
+          return Math.round(num * scaleFactor) / scaleFactor;
+        }
+      }
+    }
+  );
+})();
